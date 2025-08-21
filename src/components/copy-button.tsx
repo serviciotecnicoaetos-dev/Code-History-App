b50c6ca02f2512060ab8c2d013b9504b0b140550
serviciotecnicoaetos-dev <serviciotecnicoaetos@gmail.com>
2025-08-21 10:07:29 -0400

    feat: Implement copy to clipboard functionality

---
+++ src/components/copy-button.tsx
@@ -0,0 +1,4 @@
+export function CopyButton({ textToCopy }: { textToCopy: string }) {
+    // Dejaremos la funcionalidad para más tarde
+    return <button className="text-sm transition-colors text-zinc-500 hover:text-white">Copy</button>;
+  }
